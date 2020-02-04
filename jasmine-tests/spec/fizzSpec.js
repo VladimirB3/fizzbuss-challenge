097bb357a249a5450df639940c0ae7923ccc4442
Vladimirs Boreckis <v.boreckis@gmail.com>
2020-02-04 13:22:15 +0000

    First test written, need to come back to the challenge a little bit later after reading up on the subject

--- jasmine-tests/spec/fizzSpec.js
+++ jasmine-tests/spec/fizzSpec.js
@@ -1,14 +1,13 @@
 describe("my FizzBuzz game", function() {
 
-    beforeEach(function) {
-        number = new fizzBuzz();
-    }
+    beforeEach(function() {
+        fizzbuzz = new fizzBuzz();
+    });
 
-    describe("Check number", function() [
-        it("should be devisible by 3 and 5(14)", function() {
-            var number = (number % 3 === 0 && number % 5 === 0)
-            expect(number).toBe("FizzBuzz");
-        })
-    ])
+    describe("Check number", function() {
+        it("should exist", function() {
+            expect(fizzBuzz).toBeDefined();
+        });
+    });
 
 });
