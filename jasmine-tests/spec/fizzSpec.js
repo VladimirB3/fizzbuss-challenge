5e044523ed92294df0011e3b22ab0abefde50664
Vladimirs Boreckis <v.boreckis@gmail.com>
2020-02-04 13:08:25 +0000

    Started to write up tests

---
+++ jasmine-tests/spec/fizzSpec.js
@@ -0,0 +1,14 @@
+describe("my FizzBuzz game", function() {
+
+    beforeEach(function) {
+        number = new fizzBuzz();
+    }
+
+    describe("Check number", function() [
+        it("should be devisible by 3 and 5(14)", function() {
+            var number = (number % 3 === 0 && number % 5 === 0)
+            expect(number).toBe("FizzBuzz");
+        })
+    ])
+
+});
